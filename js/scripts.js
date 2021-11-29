344ebc06dca9a3cee7969b3752b29a40dd263194
andrewcoba <andrewcoba@gmail.com>
2021-11-29 14:09:37 -0700

    selected Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = "Meatloaf";
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Houndoom", height: "1.4", types: ["dark", "fire"] },
+  {name: "Hitmonchan", height: "1.4", type: ["fighting"] },
+  {name: "Aggron", height: "2.1", types: ["steel", "rock"] },
+];
