59fbc30a17b80c6cbc2682605514f2147ecefb85
andrewcoba <andrewcoba@gmail.com>
2021-12-15 16:47:17 -0700

    this time, done correctly

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,9 @@ let pokemonList = [
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + "(height: "+pokemonList[i].height+")");
   if (pokemonList[i].height >= 2) {
-    document.write(" - Wow, that's big!") };
-    document.write(pokemonList[i].name + "(height: "+pokemonList[i].height+")");
+    document.write(" - Wow, that's big!");
+  }
     document.write("<br/>");
 }
